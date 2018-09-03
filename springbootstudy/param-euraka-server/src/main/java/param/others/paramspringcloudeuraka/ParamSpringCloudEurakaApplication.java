74d1d1e8991c5c62d7e2702ea6400c318b978b45
paramothers <param.others@gmail.com>
2018-09-03 21:22:29 +0530

    uService study

--- springbootstudy/param-euraka-server/src/main/java/param/others/paramspringcloudeuraka/ParamSpringCloudEurakaApplication.java
+++ springbootstudy/param-euraka-server/src/main/java/param/others/paramspringcloudeuraka/ParamSpringCloudEurakaApplication.java
@@ -2,9 +2,10 @@ package param.others.paramspringcloudeuraka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurelaServer
+@EnableEurekaServer
 public class ParamSpringCloudEurakaApplication {
 
 	public static void main(String[] args) {
--- springbootstudy/param-spring-eureka-client/src/main/java/param/others/paramspringeurekaclient/ParamSpringEurekaClientApplication.java
+++ springbootstudy/param-spring-eureka-client/src/main/java/param/others/paramspringeurekaclient/ParamSpringEurekaClientApplication.java
@@ -2,15 +2,14 @@ package param.others.paramspringeurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ParamSpringEurekaClientApplication {
 
 	public static void main(String[] args) {
 		
 		SpringApplication.run(ParamSpringEurekaClientApplication.class, args);
-		System.out.println("Paramasivam ..");
 	}
 }
